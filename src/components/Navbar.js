6c9984d567d5335e81a11ca5eb74fbedd34d2184
sokainadaabal <48890714+sokainadaabal@users.noreply.github.com>
2023-05-14 13:09:42 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
 import { ImBlog } from "react-icons/im";
-import pdf from "../Assets/Cv_SokainaDaabal.pdf";
+import pdf from "../Assets/CV.pdf";
 import {
   AiFillStar,
   AiOutlineHome,
